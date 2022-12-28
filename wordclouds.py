e02c7acb3b5894fae31c1e0a289352eb2625828f
MΔS <78511249+Michael-D-Santos@users.noreply.github.com>
2022-12-28 18:55:03 -0500

    Add files used for graphing data

---
+++ wordclouds.py
@@ -0,0 +1,23 @@
+#INTENDED ON ZEN DIRECTORY
+# Import the necessary libraries
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from matplotlib.colors import ListedColormap
+
+# Create a list of words and their frequencies
+words = []
+
+# Convert the list of tuples into a dictionary
+word_dict = dict(words)
+
+# Create the wordcloud object
+wordcloud = WordCloud(width=3840, height=2160, background_color="white")
+
+# Generate the wordcloud
+wordcloud.generate_from_frequencies(word_dict)
+
+# Plot the wordcloud
+plt.figure(figsize=(8,8))
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
